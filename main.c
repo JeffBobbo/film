6485990cdd6af40c89652262df72896ddcfb41ff
JeffBobbo <james.delph@hotmail.co.uk>
2016-10-24 00:28:10 +0100

    more messing around testing stuff

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
-#include "alloc.h"
-#include "llist.h"
-
 #include <stdio.h>
 #include <stdint.h>
+
+#include "alloc.h"
+#include "llist.h"
+#include "csv.h"
 
 void runMemTest()
 {
   char* p = mt_malloc(sizeof(char) * 5);
   char* q = mt_malloc(sizeof(char) * 6);
   p[5] = 5;
-  //mt_free(p); // should emit message
-  //mt_free(q); // should be fine
+  mt_free(p); // should emit message
+  mt_free(q); // should be fine
 
   p = mt_malloc(sizeof(char) * 20);
-  q[0] = 'H';
+  p[0] = 'H';
+}
+
+bool comparison(const void* const a, const void* const b)
+{
+  return (*(int*)a > *(int*)b ? 1 : 0);
+  //return (*(int*)a < *(int*)b ? -1 : (*(int*)a > *(int*)b ? 1 : 0));
 }
 
 void llTest()
@@ -23,24 +30,41 @@ void llTest()
   for (int i = 0; i < 10; ++i)
   {
     int* p = mt_malloc(sizeof(int));
-    *p = 1 << i;
-    ll_push_front(ll, p);
+    *p = rand() % 100;
+    ll_push_back(ll, p);
   }
   printf("List size is %zu\n", ll_size(ll));
 
   LinkedIterator* it = mt_malloc(16);
   for (int* i = (int*)ll_it_begin(it, ll); i; i = (int*)ll_it_next(it))
     printf("%i\n", *i);
-  printf("And backwards...\n");
-  for (int* i = (int*)ll_it_rbegin(it, ll); i; i = (int*)ll_it_rnext(it))
+  ll_bsort(ll, &comparison);
+  printf("And sorted...\n");
+  for (int* i = (int*)ll_it_begin(it, ll); i; i = (int*)ll_it_next(it))
     printf("%i\n", *i);
   mt_free(it);
 
-  ll_delete(ll);
+  ll_purge(ll);
 }
 
 int main()
 {
   //runMemTest();
-  llTest();
+  //llTest();
+  CSV* csv = csv_read("film_short.txt");
+
+  LinkedIterator* it = mt_malloc(16);
+  LinkedList* row = (LinkedList*)ll_it_begin(it, csv->data);
+  for (; row; row = (LinkedList*)ll_it_next(it))
+  {
+    printf("Entry:\n");
+    LinkedIterator* it2 = mt_malloc(16);
+    char* d = (char*)ll_it_begin(it2, row);
+    for (; d; d = (char*)ll_it_next(it2))
+      printf("\t%s\n", d);
+    mt_free(it2);
+    ll_clear(row);
+  }
+  mt_free(it);
+  csv_free(csv);
 }
